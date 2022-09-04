8b13104d31aefc58813117e34d1cece79eccb8ac
jessicaprass <jessikprass@gmail.com>
2022-09-04 12:13:37 -0300

    calculadora segunda versão

--- calculadora.py
+++ calculadora.py
@@ -1,26 +1,44 @@
 # Programa calculadora
 # Requisitos: Este programa calcula as operações matemáticas: soma, subtração, divisão e
 # multiplicação, de dois números digitados pelo usuário e imprime o resultado na tela.
-# Autora: Jessica Caroline Prass
-# Versão: 0.0.1
-# Data: 29/08/2022
+# Autor: Jessica Caroline Prass
+# Versão: 0.0.2
+# Data: 24/08/2022
 
-# Entrada de dados
-numero_1 = float(input("\nDigite o primeiro número para calcular: "))
-numero_2 = float(input("\nDigite o segundo número para calcular: "))
-operacao = input("\nDigite a operação desejada, sendo + para somar, - para subtrair, / para dividir, * para multiplicar: ")
+# Definição de funções
+def entrada_dados():
+    numero_1 = float(input("\nDigite o primeiro número: "))
+    numero_2 = float(input("\nDigite o segundo número: "))
+    operacao = input("\nDigite a operação desejada, sendo + para somar, - para subtrair, / para dividir, * para multiplicar: ")
+    return numero_1, numero_2, operacao
+    
+def aritmetico(operacao, numero_1, numero_2):
+    if operacao == "+":
+        resultado = int(numero_1) + int(numero_2)
+    elif operacao == "-":
+        resultado = int(numero_1) - int(numero_2)
+    elif operacao == "/":
+        resultado = int(numero_1) / int(numero_2)
+    elif operacao == "*":
+        resultado = int(numero_1) * int(numero_2)
+    else:
+        print(f"\nOpção inválida.")
+        return False
+    return resultado
+        
+def imprime(operacao, numero_1, numero_2, resultado):
+    print(f"\nO resultado da operação {numero_1} {operacao} {numero_2} é igual {resultado}.")
 
-# Processa dados
-if (operacao == "+"):
-    resultado = int(numero_1) + int(numero_2)
-elif (operacao == "-"):
-    resultado = int(numero_1) - int(numero_2)
-elif (operacao == "/"):
-    resultado = int(numero_1) / int(numero_2)
-elif (operacao == "*"):
-    resultado = int(numero_1) * int(numero_2)
-else:
-    print(f"\nOpção inválida.")
+def main():
+    #Entrada de dados
+    numero_1, numero_2, operacao = entrada_dados()
 
-# Saída de dados
-print(f"\nO resultado da operação {numero_1} {operacao} {numero_2} é igual {resultado}.")
+    # Processa dados
+    resultado = aritmetico(operacao, numero_1, numero_2)
+
+    # Saída de dados
+    if resultado != False:
+        imprime(operacao, numero_1, numero_2, resultado)
+
+# Execução do programa
+main()
