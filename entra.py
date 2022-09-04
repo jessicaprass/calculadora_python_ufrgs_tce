208d77f236ce6966c6cf7ffe327201dd9aa0fb0f
jessicaprass <jessikprass@gmail.com>
2022-09-04 12:55:57 -0300

    calculadora quarta versão

--- entra.py
+++ entra.py
@@ -8,3 +8,7 @@ def entrada_numeros():
     numero_1 = float(input("\nDigite o primeiro número: "))
     numero_2 = float(input("\nDigite o segundo número: "))
     return [numero_1, numero_2]
+
+def operacao():
+    operacao = input("\nDigite a operação desejada, sendo + para somar, - para subtrair, / para dividir, * para multiplicar: ")
+    return operacao
